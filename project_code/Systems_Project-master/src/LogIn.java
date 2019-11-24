60d08dce55da1e370612671d07d9a08a02b6f016
Raivo Koot <raivokoot@gmail.com>
2019-11-24 17:15:33 +0000

    NEW NEW START

--- project_code/Systems_Project-master/src/LogIn.java
+++ project_code/Systems_Project-master/src/LogIn.java
@@ -16,8 +16,6 @@ public class LogIn extends JFrame{
     private JButton backward;
     private JButton loginButton;
     private JLabel JLabel4;
-    Connection con = null; // a Connection object
-    Statement stmt = null;
 
 
     public LogIn() {
@@ -32,93 +30,34 @@ public class LogIn extends JFrame{
                 String email = usernameInput.getText();
                 String password = psInput.getText();
 
-                int correctness = 1;
-                int validity = 1;
-
-                try {
-
-                    /*
-                    DataAccessController db = new DataAccessController(new DatabaseConstants(false));
 
                     User input = new User();
                     input.setEmail(email);
                     input.setPassword(password);
 
                     try {
-                        boolean success = db.validCredentials(input);
-                    } catch (UserAlreadyExistsException exception){
+                        boolean success = Session.db.validCredentials(input);
 
-                    } catch(IncompleteInformationException){
+                        if(success){
+                            Session.currentUser = input;
 
-                    } catch(SQLException){
-                    */
+                            // TODO: Do some UI stuff
 
-
-                    String DB="jdbc:mysql://stusql.dcs.shef.ac.uk/team035?user=team035&password=5455d7fb";
-                    con = DriverManager.getConnection(DB);
-
-                    try{
-                        //stmt = con.createStatement();
-                        PreparedStatement pstm = con.prepareStatement("SELECT * FROM `Users` WHERE `email` =? AND `password` =? AND isSuperuser = '1' AND role = 'editor'");
-                        pstm.setString(1, email);
-                        pstm.setString(2, password);
-                        PreparedStatement pstmt2 = con.prepareStatement("SELECT * FROM `Users` WHERE `email` =? AND `password` =? AND isSuperuser = '1' AND role = 'author'");
-                        pstmt2.setString(1, email);
-                        pstmt2.setString(2, password);
-                        /*PreparedStatement pstmt3 = con.prepareStatement("\"SELECT * FROM `Users` WHERE `email` =? AND `password` =? AND isSuperuser = '0' AND role = 'author'");
-                        pstmt3.setString(1, email);
-                        pstmt3.setString(2, password);
-                        PreparedStatement pstmt4 = con.prepareStatement("\"SELECT * FROM `Users` WHERE `email` =? AND `password` =? AND isSuperuser = '0' AND role = 'editor'");
-                        pstmt4.setString(1, email);
-                        pstmt4.setString(2, password);
-
-                        ResultSet rse4 = pstmt3.executeQuery();
-                        ResultSet rse3 = pstmt3.executeQuery(); */
-                        ResultSet rse2 = pstmt2.executeQuery();
-                        ResultSet rse = pstm.executeQuery();
-                        if(rse.next()){
-                                correctness = 0;
+                        } else {
+                            // TODO: Display some 'invalid login' box to the user
                         }
-                        if(rse2.next()){
-                                validity = 0;
-                        }
+                    //} catch (UserAlreadyExistsException exception){
 
-                        if(correctness == 0){
-                            ChiefEditorArea area = new ChiefEditorArea();
-                            area.setVisible(true);
-                            dispose();
-                            area.LoggedAsLabel.setText("Welcome");}
-                        else if(validity == 0){
-                            MainAuthorArea area2 = new MainAuthorArea();
-                            area2.setVisible(true);
-                            dispose();
-                        }
-                        else
-                            JLabel4.setText("Failure");
+                    //} catch(IncompleteInformationException exception){
 
-                    } catch (SQLException ex) {
-                        ex.printStackTrace();
+                    } catch(SQLException exception) {
 
-                    } finally {
-                        if (stmt != null)
-                            stmt.close();
                     }
 
 
-                } catch (SQLException ex) {
-                    ex.printStackTrace();
 
-                } finally {
-                    if (con != null) {
-                        try {
-                            con.close();
-                        } catch (SQLException e1) {
-                            e1.printStackTrace();
-                        }
-                    }
-                }
-            }
-        });
+
+        }});
 
 
 
