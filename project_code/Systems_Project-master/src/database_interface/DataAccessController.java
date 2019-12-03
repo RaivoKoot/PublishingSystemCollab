3ede82888bd88a9fcc483e36e4de11fa0ff9cb99
Klodivio355 <32410953+Klodivio355@users.noreply.github.com>
2019-12-03 21:25:09 +0000

    huge change

--- project_code/Systems_Project-master/src/database_interface/DataAccessController.java
+++ project_code/Systems_Project-master/src/database_interface/DataAccessController.java
@@ -1280,15 +1280,12 @@ public class DataAccessController implements DatabaseInterface {
           throw new InvalidAuthenticationException();
       }
 
-      if(!isMainAuthor(user, review.getSubmissionArticleID())){
-          throw new InvalidAuthenticationException();
-      }
 
       ResultSet res = null;
       try {
           openConnection();
 
-          String sqlQuery = "SELECT critiqueID, description FROM Critiques WHERE reviewID = ?";
+          String sqlQuery = "SELECT critiqueID, description, reply FROM Critiques WHERE reviewID = ?";
           statement = connection.prepareStatement(sqlQuery);
           statement.setInt(1, review.getReviewID());
 
@@ -1302,6 +1299,7 @@ public class DataAccessController implements DatabaseInterface {
               Critique critique = new Critique();
               critique.setCritiqueID(res.getInt(1));
               critique.setDescription(res.getString(2));
+              critique.setReply(res.getString(3));
 
 
               critiques.add(critique);
